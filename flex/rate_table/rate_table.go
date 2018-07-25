e041470ba16418d987746a3144785084294c1c77
Greg Russell <gfr10598@gmail.com>
2018-07-25 15:50:03 -0400

    remove dead code

--- flex/rate_table/rate_table.go
+++ flex/rate_table/rate_table.go
@@ -73,7 +73,6 @@ func receiver(w http.ResponseWriter, r *http.Request) {
 	benchmarkMemcacheGet(ctx)
 }
 
-// This shows that memcache read, with aetest environment, takes about 400 usec.
 func benchmarkMemcacheGet(ctx context.Context) {
 	ctx, err := appengine.Namespace(ctx, "memcache_requests")
 	if err != nil {
@@ -110,55 +109,3 @@ func benchmarkMemcacheGet(ctx context.Context) {
 		}
 	}
 }
-
-/*
-// TODO(gfr) unify counting for http and pubsub paths?
-func worker(rwr http.ResponseWriter, rq *http.Request) {
-	// This will add metric count and log message from any panic.
-	// The panic will still propagate, and http will report it.
-	defer func() {
-		etl.CountPanics(recover(), "worker")
-	}()
-
-	rwr.WriteHeader(http.StatusOK)
-	//, `{"message": "Success"}`
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO(soltesz): provide a real health check.
-	fmt.Fprint(w, "ok")
-}
-
-func main() {
-	// Define a custom serve mux for prometheus to listen on a separate port.
-	// We listen on a separate port so we can forward this port on the host VM.
-	// We cannot forward port 8080 because it is used by AppEngine.
-	mux := http.NewServeMux()
-	// Assign the default prometheus handler to the standard exporter path.
-	mux.Handle("/metrics", promhttp.Handler())
-	// Assign the pprof handling paths to the external port to access individual
-	// instances.
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	mux.HandleFunc("/", Status)
-	mux.HandleFunc("/status", Status)
-	go http.ListenAndServe(":9090", mux)
-
-	http.HandleFunc("/", Status)
-	http.HandleFunc("/status", Status)
-	http.HandleFunc("/worker", worker)
-	http.HandleFunc("/_ah/health", healthCheckHandler)
-
-	// Enable block profiling
-	runtime.SetBlockProfileRate(1000000) // One event per msec.
-
-	// We also setup another prometheus handler on a non-standard path. This
-	// path name will be accessible through the AppEngine service address,
-	// however it will be served by a random instance.
-	http.Handle("/random-metrics", promhttp.Handler())
-	http.ListenAndServe(":8080", nil)
-}
-*/
