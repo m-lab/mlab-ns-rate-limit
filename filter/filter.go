9ebb201b297d1459303d8e7d17f23606ec2fd38b
Gregory Russell <gfr@google.com>
2018-09-10 17:39:35 -0400

    more filter

--- filter/filter.go
+++ filter/filter.go
@@ -1,6 +1,29 @@
-// package filter manages a bloom filter on the endpoint keys
+// package filter manages a bloom filter on the endpoint keys.  Code is very simple, but it
+// is broken out to its own package to simplify testing.
 package filter
 
-func MakeEmptyFilter() *BloomFilter {
-	return
+import (
+	"log"
+
+	"github.com/willf/bloom"
+	"google.golang.org/appengine/datastore"
+)
+
+// EndpointFilter encapsulates a Bloom filter, and implements Populate function.
+type EndpointFilter struct {
+	*bloom.BloomFilter
+}
+
+// New creates an empty endpoint filter.
+func New(n uint) EndpointFilter {
+	m, k := bloom.EstimateParameters(n, 0.001)
+	log.Println("Using", m, k)
+	return EndpointFilter{bloom.New(m, k)}
+}
+
+// Populate populates the filter from a slice of keys
+func (ef *EndpointFilter) Populate(keys []*datastore.Key) {
+	for k := range keys {
+		ef.AddString(keys[k].StringID())
+	}
 }
